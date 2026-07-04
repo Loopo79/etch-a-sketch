373fb4b916b39c94c364a9630e05282b7eef04f9
Loopo79 <155375560+Loopo79@users.noreply.github.com>
2026-07-04 19:47:44 +0530

    html: add grid size input button

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const gridSizeInput = document.querySelector("#grid-size-input");
 const gridSizeForm = document.querySelector("#grid-size-form");
+const gridContainer = document.querySelector("#grid-container");
+const gridSizeInputButton = document.querySelector("#grid-size-input-button");
 
-gridSizeForm.addEventListener("submit", (e) => e.preventDefault());
+function clearGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
 
 gridSizeInput.addEventListener("keyup", (e) => {
     let gridSize = Number(gridSizeInput.value);
